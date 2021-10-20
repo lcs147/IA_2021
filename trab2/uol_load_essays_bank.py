2b4d6d5a1b147e826427a06e6c3cd9090b803ebc
lcs147 <lcs147@hotmail.com>
2021-10-20 17:24:12 -0400

    consegue baixar as redações

---
+++ trab2/uol_load_essays_bank.py
@@ -0,0 +1,16 @@
+import nltk
+import uol_redacoes_xml
+nltk.download('punkt')
+
+out = open("data/textos/textos.txt", "w")
+num_redacoes = 105
+
+essays = uol_redacoes_xml.load()[0:num_redacoes]
+total = len(essays)
+stotal = str(total)
+print("Quantidade de redacoes: " + stotal)
+
+for essay in essays:
+	print(essay.text, file=out)
+
+out.close()
